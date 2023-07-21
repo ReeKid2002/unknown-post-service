239f1749bd7bc0317d37bcc9bc73cbf66aedf83c
Rahul Behera <rahulbehera2002@gmail.com>
2023-07-22 01:25:38 +0530

    feat: add post model

---
+++ prisma/migrations/20230721195223_creating_model_for_post/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
